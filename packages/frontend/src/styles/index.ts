71849a8026583eb036077ae473447125b69dbbee
jish1 <standingbehindnv@gmail.com>
2026-02-01 13:48:12 +0900

    asdf

--- packages/frontend/src/styles/index.ts
+++ packages/frontend/src/styles/index.ts
@@ -3,6 +3,8 @@
  *
  * Components import styles from this directory using the path alias:
  * import styles from '@/styles/ComponentName.module.css'
+ *
+ * Global styles (tokens.css, animations.css) are imported in index.html
  */
 
 // Re-export individual style modules for type-safe imports
@@ -13,3 +15,9 @@ export { default as notificationCenterStyles } from './NotificationCenter.module
 export { default as toastContainerStyles } from './ToastContainer.module.css'
 export { default as windowFrameStyles } from './WindowFrame.module.css'
 export { default as renderersStyles } from './renderers.module.css'
+
+// Split modules from renderers.module.css
+export { default as componentsStyles } from './components.module.css'
+export { default as formsStyles } from './forms.module.css'
+export { default as layoutStyles } from './layout.module.css'
+export { default as typographyStyles } from './typography.module.css'
